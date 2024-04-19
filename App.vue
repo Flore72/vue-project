126ae482feb5ff845d4475cf026895c94df78b77
Flore <122017704+Flore72@users.noreply.github.com>
2024-04-19 08:13:55 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+ <div id="app">
+  <contenu></contenu>
+   </div>
+   </template>
+
+   <script>
+   import Contenu from "./components/Contenu.vue"
+
+   export default {
+    name: "App",
+    components: {
+      "contenu": Contenu
+    }
+   }
+  </script>
+
+  <style>
+  
+</style>
---
+++ Contenu.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container my-5">
+
+        <h1 class="mb-5">Une modale en Vue JS !</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Contenu"
+}
+</script>
+
+<style>
+
+</style>
